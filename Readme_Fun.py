ecf00d5ec3c45c30cae3f7d4db077f410da1e697
Zhanqian Wu <58218024+Yvonne-OH@users.noreply.github.com>
2023-12-23 15:15:31 -0500

    Add files via upload

---
+++ Readme_Fun.py
@@ -0,0 +1,12 @@
+
+def print_copyright_info():
+    copyright_text = """
+    #########################################################
+    #                                                       #
+    #                   Copyright © 2023                    #
+    #                                                       #
+    #              Author: Zhanqian Wu @ Upenn              #
+    #                                                       #
+    #########################################################
+    """
+    print(copyright_text)
